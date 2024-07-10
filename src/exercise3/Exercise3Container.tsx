66d4c2789a23c56d26d03578911082f6c49e9d74
Manolo Valdés <39590957+Manolo-Valdes@users.noreply.github.com>
2024-07-10 08:47:12 +0200

    exercise 3

--- src/exercise3/Exercise3Container.tsx
+++ src/exercise3/Exercise3Container.tsx
@@ -1,10 +1,109 @@
+import './Exercise3Container.css';
+import { ReactNode, useEffect, useState } from "react";
+import FilterDropdown from "./FilterDropDown";
 
+export interface User {
+    id:       number;
+    name:     string;
+    username: string;
+    email:    string;
+    address:  Address;
+    phone:    string;
+    website:  string;
+    company:  Company;
+}
+
+export interface Address {
+    street:  string;
+    suite:   string;
+    city:    string;
+    zipcode: string;
+    geo:     Geo;
+}
+
+export interface Geo {
+    lat: string;
+    lng: string;
+}
+
+export interface Company {
+    name:        string;
+    catchPhrase: string;
+    bs:          string;
+}
+
+export interface TriviaCategory {
+    id:   number;
+    name: string;
+}
 
 function Exercise3Container()
 {
+    const [categories, setCategories] = useState<TriviaCategory[]>([]);
+    const [selectedCategories, setSelectedCategories] = useState<TriviaCategory[]>([]);
+    const [users, setUsers] = useState<User[]>([]);
+    const [filter, setFilter] = useState<keyof User>('name');
+
+    useEffect(() => {
+        fetch("https://opentdb.com/api_category.php")
+        .then(response => response.json())
+        .then(data =>  setCategories(data.trivia_categories))
+},[]);
+    useEffect(()=>{
+        const url ='https://jsonplaceholder.typicode.com/users';
+        console.log(url);
+        fetch(url)
+        .then(response => response.json())
+        .then(json =>  {
+            const data:User[] = json;
+            setUsers(data);
+        })
+    },[]);
+ 
+    const onPropChange = (event:React.ChangeEvent<HTMLSelectElement>) =>
+        {
+            const value = event.target.value as keyof User;
+            setFilter(value);
+        }
+    
+    
+
+    function triviaToRows(items:TriviaCategory[]):ReactNode
+    {
+       return items.map((item,i) => (<tr key={i}><td>{item.id}</td><td>{item.name}</td></tr>))
+    }
+
     return(
         <>
             <h1>Exercise 3</h1>
+            <div className='row'>
+                <div className='col'>
+                    <h3>User data</h3>    
+                    <span>Find by:</span><select id="propSelect" onChange={onPropChange}>
+                        <option value="name">name</option>
+                        <option value="phone">phone</option>
+                    </select>
+                    <br/>
+                    <FilterDropdown<User> items={users} filterProp={filter} />
+                </div>
+                <div className='col'>
+                    <h3>Trivia data</h3>
+                    <FilterDropdown<TriviaCategory> items={categories} filterProp='name'  
+                        valueChange={(items)=> setSelectedCategories(items)}
+                        />
+                    </div>
+                <div className='col'>
+                <h4>Filtered Trivia data</h4>
+                <br/>
+                    <table>
+                        <thead>
+                            <tr><td>id</td><td>name</td></tr>
+                        </thead>
+                        <tbody>{triviaToRows(selectedCategories)}</tbody>
+                    </table>
+                
+            </div>
+            </div>
         </>
     );
 }
---
+++ src/exercise3/FilterDropDown.tsx
@@ -0,0 +1,89 @@
+import './FilterDropDown.css';
+import { useCallback, useEffect, useRef, useState } from "react";
+
+interface FilterDropdownProps<T>
+{
+    items:T[],
+    filterProp:keyof T,
+    valueChange?:(items:T[])=>void
+}
+
+
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+    return obj[key];
+  }
+  
+  function FilterDropdownList({items}:{items:string[]})
+  {
+    if (items.length ===0)
+        return (<></>)
+    return (<ul className="dropDown">{items.map((value,i) => <li key={i} dangerouslySetInnerHTML={{ __html:value}} />)}</ul>);
+  }  
+
+function FilterDropdown<T>({items,filterProp,valueChange=(items:T[])=>{}}:FilterDropdownProps<T>)
+{
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [filteredItems , setFilteredItems] = useState<string[]>([])
+  
+    function filterItems(filter:string):T[]
+    {
+        console.log(filter);
+        const filtered = items.filter(item => {
+            const value = getProperty(item,filterProp) as string;
+            if (value)
+                return value.toUpperCase().includes(filter.toUpperCase()) 
+            else
+                return false;
+        });
+        return filtered;
+    }
+
+    const filterCb = useCallback(filterItems,[items,filterProp])
+    useEffect(()=>{
+        const value = inputRef.current?.value;
+        if (value)
+            {
+            const filtered:T[] = filterCb(value);
+        const filteredvalues = filtered.map(item => boldPattern((getProperty(item,filterProp) as string), value));
+        setFilteredItems(filteredvalues);
+            }
+        },[filterCb,filterProp]
+        );
+
+
+    function boldPattern(value:string,pattern:string):string
+    {
+        console.log('pattern:',pattern);
+        var re = new RegExp(pattern, 'ig')
+        return (
+            value.replace(re, '<b>$&</b>')
+        )
+    }
+    function onchangeHandler()
+    {
+        const value = inputRef.current?.value;
+        console.log(value);
+        if (value)
+        {
+            const filtered:T[] = filterItems(value);
+            const filteredvalues = filtered.map(item => boldPattern((getProperty(item,filterProp) as string), value));
+            setFilteredItems(filteredvalues);
+            valueChange(filtered);
+        }
+        else
+        {
+            setFilteredItems([]);
+            valueChange([]);
+        }
+    }
+
+    return (
+        <>
+        <input ref={inputRef} type="text"  onChange={onchangeHandler}/>
+        <FilterDropdownList items={filteredItems}/>
+        </>
+    );
+}
+
+
+export default FilterDropdown;
