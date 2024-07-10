6348bc234f605bed0c8282bbb89f55332001978f
Manolo Valdés <39590957+Manolo-Valdes@users.noreply.github.com>
2024-07-10 10:25:48 +0200

    remove default unit test

--- src/exercise1/Exercise1Container.tsx
+++ src/exercise1/Exercise1Container.tsx
@@ -7,7 +7,7 @@ function Exercise1Container()
     return (
         <>
         <h1>Exercise 1</h1>
-        <p>In order to test the key listener for external app key changes. open the app in other tab, or edit directly the local Storage from the dev tools</p>
+        <p>In order to test the key listener for external apps  key changes. open the app in other tab, or edit directly the local Storage from the dev tools</p>
         <div className="row">
         <div className="col border">
             <ValueEmiter  storeKey='keyA' value='' />
